264551fc40e22e9e505ece942749247fa8c3df3c
alexander gubernikoff <agubernikoff@gmail.com>
2024-10-14 13:23:00 -0400

    services page

--- schemaTypes/servicesPage.js
+++ schemaTypes/servicesPage.js
@@ -1,14 +1,81 @@
 // schemas/servicesPage.js
-export default {
-    name: 'servicesPage',
-    title: 'Services Page',
-    type: 'document',
-    fields: [
-      {
-        name: 'hiring',
-        title: 'Hiring?',
-        type: 'boolean',
+
+import {defineField, defineType} from 'sanity'
+
+export const TYPES = [
+  {title: 'Editorial', value: 'editorial'},
+  {title: 'Consulting', value: 'consulting'},
+]
+
+const bulletPoints = defineType({
+  name: 'service',
+  title: 'Service',
+  type: 'document',
+  fields: [defineField({name: 'service', type: 'string'})],
+})
+
+const service = defineType({
+  name: 'service',
+  title: 'Service',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'ourPlaceOrYours',
+      type: 'string',
+      options: {
+        list: TYPES.map(({title, value}) => ({title, value})),
+        layout: 'radio',
       },
-    ],
-  }
-  
+    }),
+    defineField({name: 'services', type: 'array', of: [bulletPoints]}),
+    defineField({name: 'image', type: 'image'}),
+  ],
+})
+
+const bottomSquare = defineType({
+  name: 'bottomSquare',
+  title: 'Bottom Square',
+  type: 'document',
+  fields: [
+    defineField({name: 'title', type: 'string'}),
+    defineField({name: 'description', type: 'string'}),
+  ],
+})
+
+export default {
+  name: 'servicesPage',
+  title: 'Services Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'services',
+      title: 'Services',
+      type: 'array',
+      of: [service],
+      validation: (Rule) =>
+        Rule.custom((value, {document}) => {
+          if (
+            (value && value?.length > 2) ||
+            value?.filter((v) => v.ourPlaceOrYours === 'editorial').length > 1 ||
+            value?.filter((v) => v.ourPlaceOrYours === 'consulting').length > 1
+          ) {
+            return 'Please only create 1 service of type "editorial" and 1 of type "consulting"'
+          }
+          return true
+        }),
+    }),
+    defineField({
+      name: 'bottomSquares',
+      title: 'Bottom Squares',
+      type: 'array',
+      of: [bottomSquare],
+      validation: (Rule) =>
+        Rule.custom((value, {document}) => {
+          if (value && value?.length > 6) {
+            return 'Limit to 6"'
+          }
+          return true
+        }),
+    }),
+  ],
+}
