39899810e8dfe6a10fcb47c10a6cfffd96413943
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-10-14 13:41:07 -0400

    about image

--- schemaTypes/aboutPage.js
+++ schemaTypes/aboutPage.js
@@ -1,14 +1,51 @@
 // schemas/aboutPage.js
+
+const aboutBullets = defineType({
+    name: 'aboutBullets',
+    title: 'About Bullets',
+    type: 'document',
+    fields: [
+      defineField({name: 'title', type: 'string'}),
+      defineField({name: 'description', type: 'string'}),
+    ],
+  })
+
 export default {
     name: 'aboutPage',
     title: 'About Page',
     type: 'document',
     fields: [
-      {
-        name: 'hiring',
-        title: 'Hiring?',
-        type: 'boolean',
-      },
+        defineField({
+            name: 'aboutBulletsTopSection',
+            title: 'About Bullets',
+            type: 'array',
+            of: [aboutBullets],
+            // validation: (Rule) =>
+            //   Rule.custom((value, {document}) => {
+            //     if (value && value?.length > 6) {
+            //       return 'Limit to 6"'
+            //     }
+            //     return true
+            //   }),
+          }),
+          defineField({
+            name: 'image',
+            title: 'About Image',
+            type: 'image',
+          }),
+          defineField({
+            name: 'aboutBulletsBottomSection',
+            title: 'About Bullets Bottom Section',
+            type: 'array',
+            of: [aboutBullets],
+            // validation: (Rule) =>
+            //   Rule.custom((value, {document}) => {
+            //     if (value && value?.length > 6) {
+            //       return 'Limit to 6"'
+            //     }
+            //     return true
+            //   }),
+          }),
     ],
   }
   
