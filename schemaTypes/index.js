79e5be6b6c4f200bf2271956308c7d19fc276b3b
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-10-14 12:10:14 -0400

    add to index

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,5 +1,8 @@
-import siteSettings from './siteSettings'
+import teamPage from './siteSettings'
 import {clientType} from './clientType'
 import {teamMemberType} from './teamMemberType'
+import {footerPage} from './footerPage'
+import {aboutPage} from './aboutPage'
+import {servicesPage} from './servicesPage'
 
-export const schemaTypes = [clientType, teamMemberType, siteSettings]
+export const schemaTypes = [clientType, teamMemberType, teamPage, footerPage, aboutPage, servicesPage]
